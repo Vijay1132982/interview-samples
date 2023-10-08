4228efd07d2277ec1a28eae5ef43e4d92a8b9cfb
Vijay <vijayragav1132@gmail.com>
2023-10-08 13:41:37 +0530

    initial commit

---
+++ src/main/java/com/onemantech/springbootApp.java
@@ -0,0 +1,11 @@
+package com.onemantech;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class springbootApp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+}
