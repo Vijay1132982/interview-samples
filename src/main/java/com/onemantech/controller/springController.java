62867a9f43d7c14fe158db9275371c7a464e8f59
Vijay <vijayragav1132@gmail.com>
2023-10-08 13:52:45 +0530

    initial commit

--- src/main/java/com/onemantech/controller/springController.java
+++ src/main/java/com/onemantech/controller/springController.java
@@ -1,6 +1,12 @@
 package com.onemantech.controller;
 
+import com.onemantech.dao.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 @RestControllerAdvice
-public class springController {}
+public class springController {
+    @Autowired
+    Employee employee;
+}
